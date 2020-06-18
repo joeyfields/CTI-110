1449caafa9dd5b7bbc01d2c37ebc026545c9520b
joeyfields <67106966+joeyfields@users.noreply.github.com>
2020-06-18 19:58:49 +0430

    Add files via upload
    P2T1_FieldsMartin

---
+++ P2T1_SalesPrediction_FieldsMartin.py
@@ -0,0 +1,15 @@
+# A basic calculator of profit based on projected sales.
+# Date: 09Jun2020
+# CTI-110 P2T1 - Sales Prediction
+# Martin Fields
+#
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
+
+

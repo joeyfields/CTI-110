7f7dcf129b321a39eca9b49eab9763b87c3a13e2
joeyfields <67106966+joeyfields@users.noreply.github.com>
2020-06-18 19:57:53 +0430

    Add files via upload
    P1HW1_FieldsMartin

---
+++ P1HW1_FieldsMartin.py
@@ -0,0 +1,10 @@
+#This program demonstrates how the range
+# function can be used for a loop.
+
+def main ():
+    # Print a message five times.
+    for x in range (%):
+        Print ('Hello world!')
+
+# Call the main funtion.
+main ()
